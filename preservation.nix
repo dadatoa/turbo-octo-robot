21ecc05699109ff622236bb2a99c4479eb660c73
Dadatoa <f0a202@outlook.com>
2026-06-20 22:14:51 +0700

    Change preservation path from '/persistent' to '/persist'

--- preservation.nix
+++ preservation.nix
@@ -2,7 +2,7 @@
   preservation = {
     enable = true;
 
-    preserveAt."/persistent" = {
+    preserveAt."/persist" = {
       directories = [
         "/etc/nixos"
         # "/var/lib/bluetooth"
