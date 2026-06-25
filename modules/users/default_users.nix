734edb543244a06a69edb5e9fb3a414683f2a4dd
dadatoa <f0a202@outlook.com>
2026-06-25 15:07:31 +0700

    reorg ansible ymls

--- modules/users/default_users.nix
+++ modules/users/default_users.nix
@@ -12,7 +12,8 @@
     openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBA52LLKZPhszwrzrqOwLJ2a2spNzjAn/ls6krE9SM/i dadatoa@dadabook"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnWrIExo7hWe04wTUUEn6smnx/LRfNtPtatR+NgQlfz SpaceK@dadabook"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1Kg4szjDTAUgdvVrdaONcFfVnAryl+Dwira4v0gx7I operateur@xplode"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF36sv0vHnOUCx8uMWCkwLwpQoBgWP0NzYRhd6+6vr8t deploy_app_to_server_github_actions"
+
     ];
   };
   users.users.root = {
