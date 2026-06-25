f58de84f5831325b673c9085a047de1bff04611d
dadatoa <f0a202@outlook.com>
2026-06-25 18:26:58 +0700

    change disk device in vm

--- filesystems.nix
+++ filesystems.nix
@@ -7,19 +7,19 @@
 
   fileSystems."/nix" = {
     neededForBoot = true;
-    device = "/dev/xvdb";
+    device = "/dev/xvda";
     fsType = "btrfs";
     options = ["subvol=nix" "compress=zstd" "noatime"];
   };
   fileSystems."/persist" = {
     neededForBoot = true;
-    device = "/dev/xvdb";
+    device = "/dev/xvda";
     fsType = "btrfs";
     options = ["subvol=persist" "compress=zstd" "noatime"];
   };
   fileSystems."/boot" = {
     neededForBoot = true;
-    device = "/dev/xvdb";
+    device = "/dev/xvda";
     fsType = "btrfs";
     options = ["subvol=boot" "noatime"];
   };
